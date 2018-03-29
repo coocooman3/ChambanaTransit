6dd4248425a2a8d40db0a1e788e4f1416fea2af0
Ian Rasmussen <ianrsmssn@gmail.com>
2018-03-29 14:45:01 -0500

    Not really sure what I'm doing. Playing around with several UI concepts right now, may end up redoing everything.

--- ChambanaTransit/Views/MainPage.xaml.cs
+++ ChambanaTransit/Views/MainPage.xaml.cs
@@ -166,5 +166,142 @@ namespace ChambanaTransit.Views
             }
             ParsingGreenProgressIndicator.Visibility = Visibility.Collapsed;
         }
+
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
+        {
+            // you can also add items in code behind
+            NavView.MenuItems.Add(new NavigationViewItemSeparator());
+            NavView.MenuItems.Add(new NavigationViewItem()
+            { Content = "My content", Icon = new SymbolIcon(Symbol.Folder), Tag = "content" });
+
+            // set the initial SelectedItem 
+            foreach (NavigationViewItemBase item in NavView.MenuItems)
+            {
+                if (item is NavigationViewItem && item.Tag.ToString() == "home")
+                {
+                    NavView.SelectedItem = item;
+                    break;
+                }
+            }
+
+            ContentFrame.Navigated += On_Navigated;
+
+            // add keyboard accelerators for backwards navigation
+            KeyboardAccelerator GoBack = new KeyboardAccelerator();
+            GoBack.Key = VirtualKey.GoBack;
+            GoBack.Invoked += BackInvoked;
+            KeyboardAccelerator AltLeft = new KeyboardAccelerator();
+            AltLeft.Key = VirtualKey.Left;
+            AltLeft.Invoked += BackInvoked;
+            this.KeyboardAccelerators.Add(GoBack);
+            this.KeyboardAccelerators.Add(AltLeft);
+            // ALT routes here
+            AltLeft.Modifiers = VirtualKeyModifiers.Menu;
+
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked)
+            {
+                ContentFrame.Navigate(typeof(SettingsPage));
+            }
+            else
+            {
+                // find NavigationViewItem with Content that equals InvokedItem
+                var item = sender.MenuItems.OfType<NavigationViewItem>().First(x => (string)x.Content == (string)args.InvokedItem);
+                NavView_Navigate(item as NavigationViewItem);
+            }
+        }
+
+        private void NavView_Navigate(NavigationViewItem item)
+        {
+            switch (item.Tag)
+            {
+                case "home":
+                    ContentFrame.Navigate(typeof(HomePage));
+                    break;
+
+                case "apps":
+                    ContentFrame.Navigate(typeof(AppsPage));
+                    break;
+
+                case "games":
+                    ContentFrame.Navigate(typeof(GamesPage));
+                    break;
+
+                case "music":
+                    ContentFrame.Navigate(typeof(MusicPage));
+                    break;
+
+                case "content":
+                    ContentFrame.Navigate(typeof(MyContentPage));
+                    break;
+            }
+        }
+
+        private void NavView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+            On_BackRequested();
+        }
+
+        private void BackInvoked(KeyboardAccelerator sender, KeyboardAcceleratorInvokedEventArgs args)
+        {
+            On_BackRequested();
+            args.Handled = true;
+        }
+
+        private bool On_BackRequested()
+        {
+            bool navigated = false;
+
+            // don't go back if the nav pane is overlayed
+            if (NavView.IsPaneOpen && (NavView.DisplayMode == NavigationViewDisplayMode.Compact || NavView.DisplayMode == NavigationViewDisplayMode.Minimal))
+            {
+                return false;
+            }
+            else
+            {
+                if (ContentFrame.CanGoBack)
+                {
+                    ContentFrame.GoBack();
+                    navigated = true;
+                }
+            }
+            return navigated;
+        }
+
+        private void On_Navigated(object sender, NavigationEventArgs e)
+        {
+            NavView.IsBackEnabled = ContentFrame.CanGoBack;
+
+            if (ContentFrame.SourcePageType == typeof(SettingsPage))
+            {
+                NavView.SelectedItem = NavView.SettingsItem as NavigationViewItem;
+            }
+            else
+            {
+                Dictionary<Type, string> lookup = new Dictionary<Type, string>()
+        {
+            {typeof(HomePage), "home"},
+            {typeof(AppsPage), "apps"},
+            {typeof(GamesPage), "games"},
+            {typeof(MusicPage), "music"},
+            {typeof(MyContentPage), "content"}
+        };
+
+                String stringTag = lookup[ContentFrame.SourcePageType];
+
+                // set the new SelectedItem  
+                foreach (NavigationViewItemBase item in NavView.MenuItems)
+                {
+                    if (item is NavigationViewItem && item.Tag.Equals(stringTag))
+                    {
+                        item.IsSelected = true;
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
