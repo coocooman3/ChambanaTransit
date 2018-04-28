906502327314cc97f1ad0e8b13452af7dd342c8c
Ian Rasmussen <ianrsmssn@gmail.com>
2018-04-28 13:26:33 -0500

    Temp commit for rollback

--- ChambanaTransit/Views/RoutesPage.xaml.cs
+++ ChambanaTransit/Views/RoutesPage.xaml.cs
@@ -17,41 +17,41 @@ namespace ChambanaTransit.Views
             InitializeComponent();
         }
 
-        static public async void ParseRoutes()
-        {
+        //static public async void ParseRoutes()
+        //{
             
-            Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
-            Uri requestUri = new Uri("https://developer.cumtd.com/api/v2.2/json/GetRoutes?" + App.ApiKey);
+        //    Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
+        //    Uri requestUri = new Uri("https://developer.cumtd.com/api/v2.2/json/GetRoutes?" + App.ApiKey);
 
-            Windows.Web.Http.HttpResponseMessage httpResponse = new Windows.Web.Http.HttpResponseMessage();
-            string httpResponseBody = "";
+        //    Windows.Web.Http.HttpResponseMessage httpResponse = new Windows.Web.Http.HttpResponseMessage();
+        //    string httpResponseBody = "";
 
-            try
-            {
-                //Send the GET request
-                httpResponse = await httpClient.GetAsync(requestUri);
-                httpResponse.EnsureSuccessStatusCode();
-                httpResponseBody = await httpResponse.Content.ReadAsStringAsync();
-            }
-            catch (Exception ex)
-            {
-                httpResponseBody = "Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message;
-            }
-            JsonObject SortedJsonRoutesObject;
-            if (!JsonObject.TryParse(httpResponseBody, out SortedJsonRoutesObject))
-            {
-                httpResponseBody = "Error: Could not Parse Information Correctly";
-            }
-            else
-            {
-                //BusList;
-                JsonArray routeArray = SortedJsonRoutesObject.GetNamedArray("routes");
-                foreach (var route in routeArray)
-                {
-                    Routes.Add(new Route(route.GetObject()));
-                }
-            }
-        }
+        //    try
+        //    {
+        //        //Send the GET request
+        //        httpResponse = await httpClient.GetAsync(requestUri);
+        //        httpResponse.EnsureSuccessStatusCode();
+        //        httpResponseBody = await httpResponse.Content.ReadAsStringAsync();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        httpResponseBody = "Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message;
+        //    }
+        //    JsonObject SortedJsonRoutesObject;
+        //    if (!JsonObject.TryParse(httpResponseBody, out SortedJsonRoutesObject))
+        //    {
+        //        httpResponseBody = "Error: Could not Parse Information Correctly";
+        //    }
+        //    else
+        //    {
+        //        //BusList;
+        //        JsonArray routeArray = SortedJsonRoutesObject.GetNamedArray("routes");
+        //        foreach (var route in routeArray)
+        //        {
+        //            Routes.Add(new Route(route.GetObject()));
+        //        }
+        //    }
+        //}
         public event PropertyChangedEventHandler PropertyChanged;
 
         private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
