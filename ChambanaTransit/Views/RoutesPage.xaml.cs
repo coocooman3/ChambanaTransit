db54812a91b9f825d3d694b5ee2baa29fdcf0fa5
Ian Rasmussen <ianrsmssn@gmail.com>
2018-05-07 09:57:23 -0500

    Routes page is now displaying properly, had to remove static flags from variables. Need to work on reducing the routes shown, and also refactor some code.

--- ChambanaTransit/Views/RoutesPage.xaml.cs
+++ ChambanaTransit/Views/RoutesPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using ChambanaTransit.Models;
@@ -8,63 +9,50 @@ using Windows.UI.Xaml.Controls;
 
 namespace ChambanaTransit.Views
 {
-    public sealed partial class RoutesPage : Page, INotifyPropertyChanged
+    public sealed partial class RoutesPage : Page
     {
-        public static List<Route> Routes = new List<Route>();
+        public ObservableCollection<Route> Routes { get; set; } = new ObservableCollection<Route>();
 
         public RoutesPage()
         {
+            ParseRoutes();
             InitializeComponent();
         }
 
-        //static public async void ParseRoutes()
-        //{
-            
-        //    Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
-        //    Uri requestUri = new Uri("https://developer.cumtd.com/api/v2.2/json/GetRoutes?" + App.ApiKey);
-
-        //    Windows.Web.Http.HttpResponseMessage httpResponse = new Windows.Web.Http.HttpResponseMessage();
-        //    string httpResponseBody = "";
-
-        //    try
-        //    {
-        //        //Send the GET request
-        //        httpResponse = await httpClient.GetAsync(requestUri);
-        //        httpResponse.EnsureSuccessStatusCode();
-        //        httpResponseBody = await httpResponse.Content.ReadAsStringAsync();
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        httpResponseBody = "Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message;
-        //    }
-        //    JsonObject SortedJsonRoutesObject;
-        //    if (!JsonObject.TryParse(httpResponseBody, out SortedJsonRoutesObject))
-        //    {
-        //        httpResponseBody = "Error: Could not Parse Information Correctly";
-        //    }
-        //    else
-        //    {
-        //        //BusList;
-        //        JsonArray routeArray = SortedJsonRoutesObject.GetNamedArray("routes");
-        //        foreach (var route in routeArray)
-        //        {
-        //            Routes.Add(new Route(route.GetObject()));
-        //        }
-        //    }
-        //}
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        public async void ParseRoutes()
         {
-            if (Equals(storage, value))
+
+            Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
+            Uri requestUri = new Uri("https://developer.cumtd.com/api/v2.2/json/GetRoutes?" + App.ApiKey);
+
+            Windows.Web.Http.HttpResponseMessage httpResponse = new Windows.Web.Http.HttpResponseMessage();
+            string httpResponseBody = "";
+
+            try
             {
-                return;
+                //Send the GET request
+                httpResponse = await httpClient.GetAsync(requestUri);
+                httpResponse.EnsureSuccessStatusCode();
+                httpResponseBody = await httpResponse.Content.ReadAsStringAsync();
             }
-
-            storage = value;
-            OnPropertyChanged(propertyName);
+            catch (Exception ex)
+            {
+                httpResponseBody = "Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message;
+            }
+            JsonObject SortedJsonRoutesObject;
+            if (!JsonObject.TryParse(httpResponseBody, out SortedJsonRoutesObject))
+            {
+                httpResponseBody = "Error: Could not Parse Information Correctly";
+            }
+            else
+            {
+                //BusList;
+                JsonArray routeArray = SortedJsonRoutesObject.GetNamedArray("routes");
+                foreach (var route in routeArray)
+                {
+                    Routes.Add(new Route(route.GetObject()));
+                }
+            }
         }
-
-        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
